94d0aafcf2381c11e02726907eb4641429057b39
docty <=>
2020-03-18 02:25:50 +0000

    fix staff 4

--- app/Http/Controllers/MinisheetController.php
+++ app/Http/Controllers/MinisheetController.php
@@ -10,10 +10,12 @@ class MinisheetController extends Controller
 
     public function payment(Request  $request)
     {
-     
+        $baseUrl = "https://tedbus.herokuapp.com";
+       $localUrl = 'http://127.0.0.1:8100';
+       
      	$payment_schedule = new PaymentSchedule();
     	$http = new  \GuzzleHttp\Client();
-        $response = $http->get('http://127.0.0.1:8100/api/makepayment/'.$request->busId);
+        $response = $http->get($$baseUrl.'/api/makepayment/'.$request->busId);
        	$payment_schedule->busId = $request->busId;
        	$payment_schedule->amount  = $request->amount;
        	$payment_schedule->save();
