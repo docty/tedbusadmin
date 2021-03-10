ad4cc8dff7e10fe66e5e8d8010ee9cdf6ee16407
docty <=>
2021-03-10 12:51:40 +0000

    master default

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,14 +12,14 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
          DB::table('users')->insert([
-            'firstName' => 'Developer',
-            'lastName' => 'Hub',
-            'mobile' => '0242727687',
+            'firstName' => 'Test',
+            'lastName' => 'Admin',
+            'mobile' => '0241111110',
             'gender' => 'Female',
             'staffId' => '4649415',
             'urlId' => '43mdaf-dwrw-424f-d-d',
             'address' => 'AK-256-5656, Tanoso-Kumasi',
-            'email' => 'docty@test.com',
+            'email' => 'admin@test.com',
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'created_at' => now(),
